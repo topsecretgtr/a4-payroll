0fb9fbf469ec5f3216101a9b98d4eb100be3eae1
Ang Andres <andresang@gmail.com>
2023-05-11 15:55:24 +0800

    add table in employees view

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -13,8 +13,8 @@
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
+              <li class="breadcrumb-item active">{{ $title }}</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
@@ -24,66 +24,30 @@
 
     <!-- Main content -->
     <div class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div><!-- /.card -->
-          </div>
-          <!-- /.col-md-6 -->
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <!-- /.col-md-6 -->
-        </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
+      <table class="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Sex</th>
+            <th scope="col">Age</th>
+            <th scope="col">Address</th>
+            <th scope="col">Position</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($employees as $employee)
+            <tr>
+              <th scope="row">{{ $employee["id"] }}</th>
+              <td>{{ $employee["name"] }}</td>
+              <td>{{ $employee["sex"] }}</td>
+              <td>{{ $employee["age"] }}</td>
+              <td>{{ $employee["address"] }}</td>
+              <td>{{ $employee["position"] }}</td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
     </div>
     <!-- /.content -->
   </div>
--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,64 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('dashboard', [
+            "title" => 'Home'
+        ]);
 });
 
 Route::get('/employees', function () {
-    return view('employees');
+    $employees = [
+        [
+            "id" => "1",
+            "name" => "Andres",
+            "sex" => "Laki",
+            "age" => "39",
+            "address" => "monginsidi",
+            "position" => "Manager",
+        ],
+        [
+            "id" => "2",
+            "name" => "Grace",
+            "sex" => "Perempuan",
+            "age" => "39",
+            "address" => "Cut Nyak Dien",
+            "position" => "SPV",
+        ],
+        [
+            "id" => "3",
+            "name" => "Aiden",
+            "sex" => "Laki",
+            "age" => "12",
+            "address" => "Sutomo",
+            "position" => "Waiter",
+        ],
+        [
+            "id" => "4",
+            "name" => "Amelia",
+            "sex" => "Perempuan",
+            "age" => "10",
+            "address" => "Santigi",
+            "position" => "Housekeeping",
+        ],
+        [
+            "id" => "5",
+            "name" => "Adeline",
+            "sex" => "Perempuan",
+            "age" => "5",
+            "address" => "Sis Aljufri",
+            "position" => "Leader",
+        ]
+    ];
+    return view('employees', [
+        "title" => "Empployees",
+        "employees" => $employees
+    ]);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', [
+        "title" => "About"
+    ]);
 });
 
 Auth::routes();
